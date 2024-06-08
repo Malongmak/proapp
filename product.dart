e6d493aa0e76b255c08813d5f4a97a105e719a93
malong_joshua <121355936+Malongmak@users.noreply.github.com>
2024-06-08 13:50:04 +0300

    Create product.dart

---
+++ product.dart
@@ -0,0 +1,8 @@
+// lib/models/product.dart
+class Product {
+  final String name;
+  final String description;
+  final double price;
+
+  Product({required this.name, required this.description, required this.price});
+}
