dee0d26426e308f917339369df2335404a8b0d14
malong_joshua <121355936+Malongmak@users.noreply.github.com>
2024-06-08 13:52:41 +0300

    Create productlist.dart

---
+++ productlist.dart
@@ -0,0 +1,25 @@
+// lib/data/products.dart
+import 'package:your_project_name/models/product.dart';
+
+final List<Product> products = [
+  Product(
+    name: 'Pixel 1',
+    description: 'Pixel 1 is the most featureful phone ever.',
+    price: 800,
+  ),
+  Product(
+    name: 'Laptop',
+    description: 'Laptop is the most productive development tool.',
+    price: 2000,
+  ),
+  Product(
+    name: 'Tablet',
+    description: 'Tablet is the most useful device ever for meeting.',
+    price: 1500,
+  ),
+  Product(
+    name: 'Pendrive',
+    description: 'Pendrive is the stylist phone ever.',
+    price: 100,
+  ),
+];
